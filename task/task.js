2a23b46c3318b9e21d4437720feb82d715c524e5
ingalls <nick@ingalls.ca>
2020-02-21 08:06:38 -0700

    Fetch source

--- task/task.js
+++ task/task.js
@@ -1,3 +1,25 @@
 #!/usr/bin/env node
 
-console.error('HELLO WORLD');
+const require = require('request');
+
+const job = {
+    source: process.env.OA_SOURCE,
+    layer: process.env.OA_SOURCE_LAYER,
+    name: process.env.OA_SOURCE_LAYER_NAME
+}
+
+if (!job.source) throw new Error('No OA_SOURCE env var defined');
+if (!job.layer) throw new Error('No OA_SOURCE_LAYER env var defined');
+if (!job.name) throw new Error('No OA_SOURCE_LAYER_NAME env var defined');
+
+
+function fetch(job) {
+    request({
+        url: job.source,
+        method: 'GET'
+    }, (err, source) => {
+        if (err) throw err;
+
+        console.error(source);
+    });
+}
--- util/upload-lambda.sh
+++ util/upload-lambda.sh
@@ -7,7 +7,11 @@ GITSHA=$(git rev-parse HEAD)
 echo "ok - ${GITSHA}"
 
 cd $(dirname $0)/../lambda/
-zip -qrj /tmp/${GITSHA}.zip * --exclude '*node_modules*'
+
+yarn install
+
+zip -qrj /tmp/${GITSHA}.zip *
 
 aws s3 cp /tmp/${GITSHA}.zip s3://openaddresses-lambdas/batch/${GITSHA}.zip
+
 rm /tmp/${GITSHA}.zip

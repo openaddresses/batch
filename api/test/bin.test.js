0cccd5f20493432ec24826e9b9b48eb853dfa22a
ingalls <nick@ingalls.ca>
2020-03-31 19:26:27 -0700

    Add tests for Bin#match

--- api/test/bin.test.js
+++ api/test/bin.test.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Bin = require('../lib/bin');
+const Job = require('../lib/job');
 const fs = require('fs');
 const path = require('path');
 const { Pool } = require('pg');
@@ -75,6 +76,53 @@ test('Bin#update', async (t) => {
             geom: null,
             layers: ['addresses', 'buildings']
         }, 'additions retain array');
+    } catch (err) {
+        t.error(err);
+    }
+
+    pool.end();
+    t.end();
+});
+
+test('Bin#update', async (t) => {
+    const pool = new Pool({
+        connectionString: 'postgres://postgres@localhost:5432/openaddresses_test'
+    });
+
+    try {
+        await pool.query(`
+            INSERT INTO map (
+                name,
+                code,
+                layers
+            ) VALUES (
+                'Bucks County',
+                '42017',
+                '{}'
+            );
+        `);
+
+        t.deepEquals(await Bin.get_feature(pool, '42017'), {
+            name: 'Bucks County',
+            code: '42017',
+            geom: null,
+            layers: []
+        }, 'no addresses layer');
+
+        const job = new Job(
+            1,
+            'https://raw.githubusercontent.com/openaddresses/openaddresses/48ad45b0c73205457c1bfe4ff6ed7a45011d25a8/sources/us/pa/bucks.json',
+            'addresses',
+            'city'
+        );
+
+        await Bin.match(pool, job);
+        t.deepEquals(await Bin.get_feature(pool, '42017'), {
+            name: 'Bucks County',
+            code: '42017',
+            geom: null,
+            layers: ['addresses']
+        }, 'addresses layer added');
     } catch (err) {
         t.error(err);
     }

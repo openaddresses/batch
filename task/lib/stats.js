dfc8a79c7e1fd5e7d1eb957ae2c2bc1f5b70ea3f
ingalls <nick@ingalls.ca>
2020-04-12 19:41:08 -0700

    Add address layer stats

--- task/lib/stats.js
+++ task/lib/stats.js
@@ -8,14 +8,31 @@ const { pipeline } = require('stream');
 const transform = require('parallel-transform');
 
 class Stats {
-    constructor(file) {
+    constructor(file, layer) {
         if (!file || typeof file !== 'string') throw new Error('Stats.file must be a string');
+        if (!layer || typeof layer !== 'string') throw new Error('Stats.layer must be a string');
 
         this.file = file;
+        this.layer = layer;
+
         this.stats = {
             count: 0,
             bounds: []
         };
+
+        if (this.layer === 'addresses') {
+            this.stats.addresses = {
+                counts: {
+                    unit: 0,
+                    number: 0,
+                    street: 0,
+                    city: 0,
+                    district: 0,
+                    region: 0,
+                    postcode: 0
+                }
+            };
+        }
     }
 
     calc() {
@@ -26,16 +43,22 @@ class Stats {
                 transform(100, (data, cb) => {
                     if (!data.trim().length) return cb(null, '');
 
+                    let feat;
                     try {
-                        const feat = JSON.parse(data);
-                        this.stats.count++;
-
-                        this.bounds(feat);
-
-                        return cb(null, '');
+                        feat = JSON.parse(data);
                     } catch (err) {
                         return reject(err);
                     }
+
+                    this.stats.count++;
+
+                    this.bounds(feat);
+
+                    if (this.layer === 'addresses') {
+                        this.addresses(feat);
+                    }
+
+                    return cb(null, '');
                 }),
                 fs.createWriteStream('/dev/null'),
                 (err) => {
@@ -45,6 +68,16 @@ class Stats {
                 }
             );
         });
+    }
+
+    addresses(feat) {
+        if (feat.properties.unit.trim().length > 0) this.stats.addresses.counts.unit++;
+        if (feat.properties.number.trim().length > 0) this.stats.addresses.counts.number++;
+        if (feat.properties.street.trim().length > 0) this.stats.addresses.counts.street++;
+        if (feat.properties.city.trim().length > 0) this.stats.addresses.counts.city++;
+        if (feat.properties.district.trim().length > 0) this.stats.addresses.counts.district++;
+        if (feat.properties.region.trim().length > 0) this.stats.addresses.counts.region++;
+        if (feat.properties.postcode.trim().length > 0) this.stats.addresses.counts.postcode++;
     }
 
     bounds(feat) {

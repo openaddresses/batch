e574012a22d2b5a26e770eb7b364770e98fd1887
ingalls <nick@ingalls.ca>
2020-04-20 12:49:18 -0700

    Add registration

--- api/web/src/components/Register.vue
+++ api/web/src/components/Register.vue
@@ -5,6 +5,16 @@
                 <div class='flex-child loading py24'></div>
             </div>
         </template>
+        <template v-else-if='success'>
+            <div class='col col--12 flex-parent flex-parent--center-main'>
+                <h3 class='flex-child txt-h4 py6'>Successfully Registered!</h3>
+            </div>
+            <div class='col col--12 flex-parent flex-parent--center-main'>
+                <div class='w240 col col--12 grid grid--gut12'>
+                    <button @click='login' class='mt12 w-full color-gray color-green-on-hover btn btn--stroke round'>Login</button>
+                </div>
+            </div>
+        </template>
         <template v-else>
             <div class='col col--12 flex-parent flex-parent--center-main'>
                 <h3 class='flex-child txt-h4 py6'>Register</h3>
@@ -13,13 +23,19 @@
             <div class='col col--12 flex-parent flex-parent--center-main'>
                 <div class='w240 col col--12 grid grid--gut12'>
                     <label class='mt12'>Username:</label>
-                    <input v-model='username' type='text' class='input'/>
+                    <input :class='{
+                        "input--border-red": attempted && !username
+                    }' v-model='username' type='text' class='input'/>
 
                     <label class='mt12'>Email:</label>
-                    <input v-model='email' type='text' class='input'/>
+                    <input :class='{
+                        "input--border-red": attempted && !email
+                    }' v-model='email' type='text' class='input'/>
 
                     <label class='mt12'>Password:</label>
-                    <input v-model='password' type='password' class='input'/>
+                    <input :class='{
+                        "input--border-red": attempted && !password
+                    }' v-model='password' type='password' class='input'/>
 
                     <button @click='register' class='mt12 w-full color-gray color-green-on-hover btn btn--stroke round'>Register</button>
                 </div>
@@ -35,6 +51,8 @@ export default {
     props: [],
     data: function() {
         return {
+            success: false,
+            attempted: false,
             loading: false,
             email: '',
             username: '',
@@ -42,9 +60,18 @@ export default {
         }
     },
     methods: {
+        login: function() {
+            this.$router.push('/login');
+        },
         register: function() {
+            this.attempted = true;
+
+            if (!this.username.length) return;
+            if (!this.password.length) return;
+            if (!this.email.length) return;
             this.loading = true;
 
+            let status;
             fetch(window.location.origin + `/api/user`, {
                 method: 'POST',
                 headers: {
@@ -56,8 +83,20 @@ export default {
                     email: this.email,
                     password: this.password
                 })
-            }).then(() => {
+            }).then((res) => {
+                status = res.status;
+                return res.json();
+            }).then((res) => {
                 this.loading = false;
+                if (status !== 200 && res.message) {
+                    throw new Error(res.message);
+                } else if (status !== 200) {
+                    throw new Error('Failed to register user');
+                }
+
+                this.success = true;
+            }).catch((err) => {
+                this.$emit('err', err);
             });
         }
     }

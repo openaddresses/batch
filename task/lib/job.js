e22f33b9050a9ed4aacc4e6b3b2edff15f74ec59
ingalls <nick@ingalls.ca>
2020-09-22 17:34:17 -0700

    l => layer

--- task/lib/job.js
+++ task/lib/job.js
@@ -306,8 +306,8 @@ class Job {
             if (l.name === this.name) layer = l;
         }
 
-        if (l.skip) await JobError.create(api, this.job, `Job has skip: true flag enabled`);
-        if (l.year && parseInt(l.year) !== new Date().getFullYear()) await JobError.create(api, this.job, `Job has year: ${l.year} which is not the current year`);
+        if (layer.skip) await JobError.create(api, this.job, `Job has skip: true flag enabled`);
+        if (layer.year && parseInt(layer.year) !== new Date().getFullYear()) await JobError.create(api, this.job, `Job has year: ${layer.year} which is not the current year`);
 
         return true;
     }

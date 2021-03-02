2114e48d06a496bb1afdddb9c32893a567ac674b
ingalls <nick@ingalls.ca>
2021-03-01 20:49:30 -0700

    Add users.level

--- api/schema.sql
+++ api/schema.sql
@@ -20,6 +20,7 @@ CREATE INDEX IF NOT EXISTS idx_session_expire ON session ("expire");
 CREATE TABLE IF NOT EXISTS users (
     id          BIGSERIAL PRIMARY KEY,
     created     TIMESTAMP DEFAULT NOW(),
+    level       TEXT NOT NULL DEFAULT 'basic',
     access      TEXT NOT NULL,
     flags       JSONB NOT NULL,
     username    TEXT UNIQUE NOT NULL,

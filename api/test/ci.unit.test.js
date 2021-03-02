b912f08f84f422107a35385d33e55a0c9a03ad49
ingalls <nick@ingalls.ca>
2021-03-01 19:42:31 -0700

    Add internaldiff fn

--- api/test/ci.unit.test.js
+++ api/test/ci.unit.test.js
@@ -2,47 +2,80 @@
 
 const CI = require('../lib/ci');
 const test = require('tape');
+const nock = require('nock');
 
 test('CI()', (t) => {
     t.end();
 });
 
-test('CI#fileprep', (t) => {
-    t.deepEquals(CI.fileprep([
-        'sources/us/ca/orange.json'
-    ], '1234'), [
-        'https://raw.githubusercontent.com/openaddresses/openaddresses/1234/sources/us/ca/orange.json'
-    ], 'single unique file');
+test('CI#internaldiff - No File On Master', async (t) => {
+    nock('https://raw.githubusercontent.com')
+        .get('/openaddresses/openaddresses/123/sources/us/mt/statewide.json')
+            .reply(200, JSON.stringify({
+                schema: 2,
+                layers: {
+                    addresses: [{ name: 'state' }]
+                }
+            }))
+        .get('/openaddresses/openaddresses/master/sources/us/mt/statewide.json')
+            .reply(404, 'NOT FOUND');
 
-    t.deepEquals(CI.fileprep([
-        'sources/ca/ca/orange.json',
-        'sources/us/ca/orange.json'
-    ], '1234'), [
-        'https://raw.githubusercontent.com/openaddresses/openaddresses/1234/sources/ca/ca/orange.json',
-        'https://raw.githubusercontent.com/openaddresses/openaddresses/1234/sources/us/ca/orange.json'
-    ], 'multi unique files');
+    const jobs = await CI.internaldiff([{
+        filename: 'sources/us/mt/statewide.json',
+        raw: 'https://raw.githubusercontent.com/openaddresses/openaddresses/123/sources/us/mt/statewide.json'
+    }]);
 
-    t.deepEquals(CI.fileprep([
-        'package.json',
-        'sources/ca/ca/orange.json',
-        'test/test.js',
-        'sources/us/ca/orange.json'
-    ], '1234'), [
-        'https://raw.githubusercontent.com/openaddresses/openaddresses/1234/sources/ca/ca/orange.json',
-        'https://raw.githubusercontent.com/openaddresses/openaddresses/1234/sources/us/ca/orange.json'
-    ], 'multi unique files w/ non-sources');
+    t.deepEquals(jobs, [{
+        source: 'https://raw.githubusercontent.com/openaddresses/openaddresses/123/sources/us/mt/statewide.json',
+        layer: 'addresses',
+        name: 'state'
+    }]);
 
-    t.deepEquals(CI.fileprep([
-        'sources/us/ca/orange.json',
-        'package.json',
-        'sources/ca/ca/orange.json',
-        'test/test.js',
-        'sources/us/ca/orange.json',
-        'sources/ca/ca/orange.json'
-    ], '1234'), [
-        'https://raw.githubusercontent.com/openaddresses/openaddresses/1234/sources/ca/ca/orange.json',
-        'https://raw.githubusercontent.com/openaddresses/openaddresses/1234/sources/us/ca/orange.json'
-    ], 'multi unique files w/ non-sources & dupes');
+    t.end();
+});
 
+test('CI#internaldiff - Internal Diff', async (t) => {
+    nock('https://raw.githubusercontent.com')
+        .get('/openaddresses/openaddresses/123/sources/us/mt/statewide.json')
+            .reply(200, JSON.stringify({
+                schema: 2,
+                layers: {
+                    addresses: [{ name: 'state' }, { name: 'state-other' }, { name: 'no-diff' }],
+                    parcels: [{ name: 'state' }]
+                }
+            }))
+        .get('/openaddresses/openaddresses/master/sources/us/mt/statewide.json')
+            .reply(200, JSON.stringify({
+                schema: 2,
+                layers: {
+                    addresses: [{ name: 'state', diff: true }, { name: 'no-diff' }]
+                }
+            }))
+
+    const jobs = await CI.internaldiff([{
+        filename: 'sources/us/mt/statewide.json',
+        raw: 'https://raw.githubusercontent.com/openaddresses/openaddresses/123/sources/us/mt/statewide.json'
+    }]);
+
+    t.deepEquals(jobs, [{
+        source: 'https://raw.githubusercontent.com/openaddresses/openaddresses/123/sources/us/mt/statewide.json',
+        layer: 'addresses',
+        name: 'state'
+    },{
+        source: 'https://raw.githubusercontent.com/openaddresses/openaddresses/123/sources/us/mt/statewide.json',
+        layer: 'addresses',
+        name: 'state-other'
+    },{
+        source: 'https://raw.githubusercontent.com/openaddresses/openaddresses/123/sources/us/mt/statewide.json',
+        layer: 'parcels',
+        name: 'state'
+    }]);
+
+    t.end();
+});
+
+test('close', (t) => {
+    nock.cleanAll();
+    nock.enableNetConnect();
     t.end();
 });

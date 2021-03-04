92e7982c87c604e0b336d18d713d95a8ebd21f07
ingalls <nick@ingalls.ca>
2021-03-04 12:59:29 -0700

    Get users list

--- api/lib/level.js
+++ api/lib/level.js
@@ -18,6 +18,9 @@ class Level {
     /**
      * Check the Level of a given user
      *
+     * TODO: https://github.com/opencollective/opencollective-api/pull/5561
+     *      will allow us to only have to query for a single user instead of a list
+     *
      * @param {User} user
      */
     async get_user(user) {
@@ -29,12 +32,60 @@ class Level {
                 'Api-Key': this.OpenCollective
             },
             body: {
-
-            }
+                query: `{
+                    account(slug: "openaddresses") {
+                      members {
+                        nodes {
+                          id
+                          role
+                          account {
+                            id
+                            slug
+                            ... on Individual {
+                              email
+                            }
+                         }
+                      }
+                    }
+                  }
+                }`
+             }
         });
+    }
 
-        console.error(res.body);
+    /**
+     * Refresh the entire user list
+     */
+    async all() {
+        const res = await request({
+            url: this.base,
+            method: 'POST',
+            json: true,
+            headers: {
+                'Api-Key': this.OpenCollective
+            },
+            body: {
+                query: `{
+                    account(slug: "openaddresses") {
+                      members {
+                        nodes {
+                          id
+                          role
+                          account {
+                            id
+                            slug
+                            ... on Individual {
+                              email
+                            }
+                         }
+                      }
+                    }
+                  }
+                }`
+             }
+        });
     }
 }
+
 
 module.exports = Level;

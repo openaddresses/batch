f8c6900f6ad1e09aaeb84f036922c7435c3a04b7
ingalls <nick@ingalls.ca>
2021-03-16 18:04:15 -0600

    Fix unix stamp

--- api/test/bunny.unit.test.js
+++ api/test/bunny.unit.test.js
@@ -2,13 +2,12 @@
 
 const Bunny = require('../lib/bunny');
 const tape = require('tape');
-const moment = require('moment');
 
 tape('Bunny#sign', (t) => {
     const bunny = new Bunny('123');
 
     t.equals(
-        bunny.sign('https://v2.openaddresses.io/batch-prod/collection-global.zip', moment('2021-01-01').unix()),
+        bunny.sign('https://v2.openaddresses.io/batch-prod/collection-global.zip', 1609484400),
         'https://v2.openaddresses.io/batch-prod/collection-global.zip?token=82EYgkUPhv%2FL9szDzOkUU8Lqw4m8%2F35j3qXyX0zXaX4%3D&expires=1609484400',
         'expected token'
     );

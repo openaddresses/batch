4bd563d476e4a1ed8bc9d806ec5ef9b7832516f0
ingalls <nick@mapbox.com>
2021-07-03 15:29:42 -0600

    Slightly nicer 404 page

--- api/web/src/components/NotFound.vue
+++ api/web/src/components/NotFound.vue
@@ -1,6 +1,10 @@
 <template>
-    <div class='col col--12 grid pt12'>
-        PAGE NOT FOUND
+    <div class='col col--12 py24'>
+        <div class='align-center'>We couldn't find that page!</div>
+
+        <div class='flex flex--center-main py24'>
+            <button @click='$router.push("/data")' class='color-gray btn btn--stroke round'>Home</button>
+        </div>
     </div>
 </template>
 

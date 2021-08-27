2616aea4efcde61d755629d5c72c03357a2d97c4
ingalls <nick@ingalls.ca>
2021-08-27 13:50:59 -0600

    Fix tests

--- api/doc/api_project.js
+++ api/doc/api_project.js
@@ -12,7 +12,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-08-26T18:23:33.714Z",
+    "time": "2021-08-27T19:50:10.034Z",
     "url": "https://apidocjs.com",
     "version": "0.29.0"
   }

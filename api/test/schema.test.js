e85dca31750fe213e6fedf1a8c851c6c270dacb0
ingalls <nick@ingalls.ca>
2021-08-26 21:09:01 -0600

    Add stronger schema validation tests

--- api/test/schema.test.js
+++ api/test/schema.test.js
@@ -5,14 +5,28 @@ const path = require('path');
 const test = require('tape');
 
 const glob = require('glob');
+const $RefParser = require('json-schema-ref-parser');
+const Ajv = require('ajv');
+
+const ajv = new Ajv({
+    allErrors: true
+});
 
 glob.sync(path.resolve(__dirname, '../schema/**/*.json')).forEach((source) => {
-    test(`schema/${path.parse(source).base}`, (t) => {
-        const file = fs.readFileSync(source);
-        t.ok(file.length, 'file loaded');
+    test(`schema/${path.parse(source).base}`, async (t) => {
+        try {
+            const file = fs.readFileSync(source);
+            t.ok(file.length, 'file loaded');
+
+            JSON.parse(file);
+        } catch (err) {
+            t.error(err, 'no JSON errors');
+        }
 
         try {
-            JSON.parse(file);
+            const schema = await $RefParser.dereference(source)
+
+            ajv.compile(schema);
         } catch (err) {
             t.error(err, 'no errors');
         }

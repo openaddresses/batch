baa15a50d50799ae74dfb8fa1aa9c3a0e5795ffd
ingalls <nick@ingalls.ca>
2021-11-02 13:08:53 -0700

    Add health & 404 test

---
+++ api/test/health.srv.test.js
@@ -0,0 +1,31 @@
+const test = require('tape');
+const Flight = require('./flight');
+const { sql } = require('slonik');
+const moment = require('moment');
+
+const flight = new Flight();
+flight.init(test);
+flight.takeoff(test);
+
+test('GET: /health', async (t) => {
+    try {
+        const res = await flight.request({
+            url: '/health',
+            method: 'GET',
+            json: true
+        }, false);
+
+        t.equals(res.statusCode, 200, 'http: 200');
+        t.deepEquals(res.body, {
+            healthy: true,
+            message: 'I work all day, I work all night to get the open the data!'
+        });
+
+    } catch (err) {
+        t.error(err, 'no error');
+    }
+
+    t.end();
+});
+
+flight.landing(test);

ffaa2e15073c2b2b7a31f4499cb96077fe503096
ingalls <nick@ingalls.ca>
2022-12-16 11:11:31 -0800

    Fix fetch mocks

--- api/test/ci.unit.test.js
+++ api/test/ci.unit.test.js
@@ -1,25 +1,36 @@
 import CI from '../lib/ci.js';
 import test from 'node:test';
 import assert from 'assert';
+import { MockAgent, setGlobalDispatcher } from 'undici';
 
 test('CI#internaldiff - No File On Master', async () => {
+    const mockAgent = new MockAgent();
+    const mockPool = mockAgent.get('https://raw.githubusercontent.com');
+    setGlobalDispatcher(mockAgent)
+
+    mockPool.intercept({
+        path: '/openaddresses/openaddresses/123/sources/us/mt/statewide.json'
+    }).reply(200, {
+        schema: 2,
+        layers: {
+            addresses: [{ name: 'state' }]
+        }
+    }, {
+         headers: { 'content-type': 'application/json' }
+    });
+
+    mockPool.intercept({
+        path: '/openaddresses/openaddresses/master/sources/us/mt/statewide.json'
+    }).reply(404, {
+        status: 404
+    }, {
+         headers: { 'content-type': 'application/json' }
+    });
+
     const jobs = await CI.internaldiff([{
         filename: 'sources/us/mt/statewide.json',
         raw: 'https://raw.githubusercontent.com/openaddresses/openaddresses/123/sources/us/mt/statewide.json'
-    }], async (url) => {
-        if (new URL(url).pathname === '/openaddresses/openaddresses/123/sources/us/mt/statewide.json') {
-            return new Response(JSON.stringify({
-                schema: 2,
-                layers: {
-                    addresses: [{ name: 'state' }]
-                }
-            }));
-        } else if (new URL(url).pathname === '/openaddresses/openaddresses/master/sources/us/mt/statewide.json') {
-            return new Response('NOT FOUND', {
-                status: 404
-            });
-        }
-    });
+    }]);
 
     assert.deepEqual(jobs, [{
         source: 'https://raw.githubusercontent.com/openaddresses/openaddresses/123/sources/us/mt/statewide.json',
@@ -29,28 +40,37 @@ test('CI#internaldiff - No File On Master', async () => {
 });
 
 test('CI#internaldiff - Internal Diff', async () => {
+    const mockAgent = new MockAgent();
+    const mockPool = mockAgent.get('https://raw.githubusercontent.com');
+    setGlobalDispatcher(mockAgent)
+
+    mockPool.intercept({
+        path: '/openaddresses/openaddresses/123/sources/us/mt/statewide.json'
+    }).reply(200, {
+        schema: 2,
+        layers: {
+            addresses: [{ name: 'state' }, { name: 'state-other' }, { name: 'no-diff' }],
+            parcels: [{ name: 'state' }]
+        }
+    }, {
+         headers: { 'content-type': 'application/json' }
+    });
+
+    mockPool.intercept({
+        path: '/openaddresses/openaddresses/master/sources/us/mt/statewide.json'
+    }).reply(200, {
+        schema: 2,
+        layers: {
+            addresses: [{ name: 'state', diff: true }, { name: 'no-diff' }]
+        }
+    }, {
+         headers: { 'content-type': 'application/json' }
+    });
+
     const jobs = await CI.internaldiff([{
         filename: 'sources/us/mt/statewide.json',
         raw: 'https://raw.githubusercontent.com/openaddresses/openaddresses/123/sources/us/mt/statewide.json'
-    }], async (url) => {
-        if (new URL(url).pathname === '/openaddresses/openaddresses/123/sources/us/mt/statewide.json') {
-            return new Response(JSON.stringify({
-                schema: 2,
-                layers: {
-                    addresses: [{ name: 'state' }, { name: 'state-other' }, { name: 'no-diff' }],
-                    parcels: [{ name: 'state' }]
-                }
-            }));
-        } else if (new URL(url).pathname === '/openaddresses/openaddresses/master/sources/us/mt/statewide.json') {
-            return new Response(JSON.stringify({
-                schema: 2,
-                layers: {
-                    addresses: [{ name: 'state', diff: true }, { name: 'no-diff' }]
-                }
-            }));
-        }
-    });
-
+    }]);
     assert.deepEqual(jobs, [{
         source: 'https://raw.githubusercontent.com/openaddresses/openaddresses/123/sources/us/mt/statewide.json',
         layer: 'addresses',

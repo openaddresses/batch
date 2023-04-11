d53b56da648c9859264663973074b0961cd357ab
ingalls <nick@ingalls.ca>
2023-04-11 16:16:24 -0600

    mapbox@cloudfriend => openaddresses/cloudfriend

--- cloudformation/batch.template.js
+++ cloudformation/batch.template.js
@@ -1,4 +1,4 @@
-import cf from '@mapbox/cloudfriend';
+import cf from '@openaddresses/cloudfriend';
 import alarms from '@openaddresses/batch-alarms';
 
 import schedule from './lib/schedule.js';
--- cloudformation/lib/schedule.js
+++ cloudformation/lib/schedule.js
@@ -1,4 +1,4 @@
-import cf from '@mapbox/cloudfriend';
+import cf from '@openaddresses/cloudfriend';
 
 export default function schedule(type, cron, desc) {
     const stack =  {

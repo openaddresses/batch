0b1fc2bce4e4fd3afef32d9f22a72743e63d1c08
ingalls <nick@ingalls.ca>
2023-09-01 15:51:54 -0600

    Get Jobs in a Table

--- api/web/src/components/Jobs.vue
+++ api/web/src/components/Jobs.vue
@@ -1,78 +1,87 @@
 <template>
-    <div class='col col--12 grid pt12'>
-        <div class='col col--12 grid border-b border--gray-light'>
-            <div class='col col--12'>
-                <h2 class='txt-h4 pb12 fl'>Jobs:</h2>
-
-                <button @click='refresh' class='btn round btn--stroke fr color-gray'>
-                    <svg class='icon'><use xlink:href='#icon-refresh'/></svg>
-                </button>
-            </div>
-
-            <div class='col col--1'>
-                Status
-            </div>
-            <div class='col col--2'>
-                Job ID
-            </div>
-            <div class='col col--4'>
-                Source
-            </div>
-            <div class='col col--5'>
-                <span class='fr'>Attributes</span>
-            </div>
-        </div>
-
-        <template v-if='loading'>
-            <div class='flex flex--center-main w-full py24'>
-                <div class='loading'></div>
-            </div>
-        </template>
-        <template v-else>
-            <div :key='job.id' v-for='job in jobs' class='col col--12 grid'>
-                <div @click='emitjob(job.id)' class='col col--12 grid py12 cursor-pointer bg-darken10-on-hover round'>
-                    <div class='col col--1'>
-                        <Status :status='job.status'/>
-                    </div>
-                    <div class='col col--2'>
-                        Job <span v-text='job.id'/>
-                    </div>
-                    <div class='col col--4'>
-                        <span v-text='`${job.source_name} - ${job.layer} - ${job.name}`'/>
-                    </div>
-                    <div class='col col--5 pr12'>
-                        <Download :auth='auth' :job='job' @login='$emit("login")' @perk='$emit("perk", $event)'/>
-
-                        <span v-on:click.stop.prevent='external(job.source)' class='fr h24 cursor-pointer mx3 px12 round color-gray border border--transparent border--gray-on-hover'>
-                            <BrandGithubIcon width="16" height="16"/>
-                        </span>
-
-                        <span v-on:click.stop.prevent='emitlog(job.id)' v-if='job.loglink' class='fr h24 cursor-pointer mx3 px12 round color-gray border border--transparent border--gray-on-hover'>
-                            <NotesIcon width="16" height="16"/>
-                        </span>
+<div>
+    <div class='page-wrapper'>
+        <div class="page-header d-print-none">
+            <div class="container-xl">
+                <div class="row g-2 align-items-center">
+                    <div class="col d-flex">
+                        <TablerBreadCrumb/>
                     </div>
                 </div>
             </div>
-        </template>
+        </div>
     </div>
+    <div class='page-body'>
+        <div class='container-xl'>
+            <div class='row row-deck row-cards'>
+                <div class='col-12'>
+                    <div class='card'>
+                        <div class='card-header'>
+                            <h3 class='card-title'>Jobs</h3>
+
+                            <div class='ms-auto btn-list'>
+                                <RefreshIcon @click='fetchJobs' class='cursor-pointer'/>
+                            </div>
+                        </div>
+
+                        <TablerLoading v-if='loading' desc='Loading Jobs'/>
+                        <table v-else class="table table-hover table-vcenter card-table">
+                            <thead>
+                                <tr>
+                                    <th>Status</th>
+                                    <th>Job ID</th>
+                                    <th>Created</th>
+                                    <th>Source</th>
+                                    <th>Attributes</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr @click='$router.push(`/job/${job.id}`);' :key='job.id' v-for='job in jobs' class='cursor-pointer'>
+                                    <td><Status :status='job.status'/></td>
+                                    <td>Run <span v-text='job.id'/></td>
+                                    <td><span v-text='fmt(job.created)'/></td>
+                                    <td>
+                                        <span v-text='`${job.source_name} - ${job.layer} - ${job.name}`'/>
+                                    </td>
+                                    <td>
+                                        <Download :auth='auth' :job='job' @login='$emit("login")' @perk='$emit("perk", $event)'/>
+                                        <span v-on:click.stop.prevent='external(job.source)' class='fr h24 cursor-pointer mx3 px12 round color-gray border border--transparent border--gray-on-hover'>
+                                            <BrandGithubIcon width="16" height="16"/>
+                                        </span>
+                                        <span v-on:click.stop.prevent='$router.push(`/job/${job.id}/log`)' v-if='job.loglink' class='fr h24 cursor-pointer mx3 px12 round color-gray border border--transparent border--gray-on-hover'>
+                                            <NotesIcon width="16" height="16"/>
+                                        </span>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 </template>
 
 <script>
 import {
     BrandGithubIcon,
+    RefreshIcon,
     NotesIcon
 } from 'vue-tabler-icons'
 import Status from './util/Status.vue';
 import Download from './Download.vue';
+import moment from 'moment-timezone';
 
 export default {
     name: 'Jobs',
     props: [ 'auth' ],
     mounted: function() {
-        this.refresh();
+        this.fetchJobs();
     },
     data: function() {
         return {
+            tz: moment.tz.guess(),
             jobs: [],
             loading: false
         };
@@ -81,16 +90,10 @@ export default {
         external: function(url) {
             window.open(url, "_blank");
         },
-        emitlog: function(jobid) {
-            this.$router.push({ path: `/job/${jobid}/log` });
+        fmt: function(date) {
+            return moment(date).tz(this.tz).format('YYYY-MM-DD hh:mm');
         },
-        emitjob: function(jobid) {
-            this.$router.push({ path: `/job/${jobid}` });
-        },
-        refresh: function() {
-            this.getJobs();
-        },
-        getJobs: async function() {
+        fetchJobs: async function() {
             try {
                 this.loading = true;
                 this.jobs = await window.std(window.location.origin + '/api/job');
@@ -102,6 +105,7 @@ export default {
     },
     components: {
         Download,
+        RefreshIcon,
         Status,
         BrandGithubIcon,
         NotesIcon

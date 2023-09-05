85de16aa7946ae0f44dad2752f8cffad40a5c36d
ingalls <nick@ingalls.ca>
2023-09-05 12:29:43 +0100

    Add BreadCrumb to Location

--- api/web/src/components/Location.vue
+++ api/web/src/components/Location.vue
@@ -1,67 +1,80 @@
 <template>
-<div class='page-body'>
-    <div class='container-xl'>
-        <div class='row row-deck row-cards'>
-            <div class='col-12'>
-                <div class='card'>
-                    <div class='card-header'>
-                        <h3 class='card-title' v-text='location.name'></h3>
+<div>
+    <div class='page-wrapper'>
+        <div class="page-header d-print-none">
+            <div class="container-xl">
+                <div class="row g-2 align-items-center">
+                    <div class="col d-flex">
+                        <TablerBreadCrumb/>
                     </div>
-                    <div class='card-body row'>
-                        <TablerLoading v-if='loading'/>
-                        <template v-else>
-                            <button @click='$router.go(-1)' class='btn round btn--stroke fl color-gray'>
-                                <svg class='icon'><use xlink:href='#icon-arrow-left'/></svg>
-                            </button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class='page-body'>
+        <div class='container-xl'>
+            <div class='row row-deck row-cards'>
+                <div class='col-12'>
+                    <div class='card'>
+                        <div class='card-header'>
+                            <h3 class='card-title' v-text='location.name'></h3>
+                        </div>
+                        <div class='card-body row'>
+                            <TablerLoading v-if='loading'/>
+                            <template v-else>
+                                <button @click='$router.go(-1)' class='btn round btn--stroke fl color-gray'>
+                                    <svg class='icon'><use xlink:href='#icon-arrow-left'/></svg>
+                                </button>
 
-                            <div class='col-12'>
-                                <Coverage
-                                    @err='$emit("err", $event)'
-                                    :filter='locid'
-                                    :bbox='location.bbox'
-                                />
-                            </div>
+                                <div class='col-12'>
+                                    <Coverage
+                                        @err='$emit("err", $event)'
+                                        :filter='locid'
+                                        :bbox='location.bbox'
+                                    />
+                                </div>
 
-                            <table class="table table-vcenter card-table">
-                                <thead>
-                                    <tr>
-                                        <th>Name</th>
-                                        <th>Updated</th>
-                                        <th>Size</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr :key='job.id' v-for='job in jobs'>
-                                        <td @click='emitjob(job.job)'>
-                                            <span v-text='job.layer'/> - <span v-text='job.name'/>
-                                        </td>
-                                        <td v-text='fmt(job.updated)'></td>
-                                        <td class='d-flex'>
-                                            <span v-text='size(job.size)'/>
-                                            <div class='ms-auto btn-list'>
-                                                <Download :auth='auth' :job='job' @login='$emit("login")' @perk='$emit("perk", $event)'/>
-                                                <template v-if='auth && auth.access === "admin"'>
-                                                    <TablerDropdown>
-                                                        <slot>
-                                                            <SettingsIcon class='cursor-pointer'/>
-                                                        </slot>
-                                                        <template #dropdown>
-                                                            <TablerToggle @change='updateData(job)' v-model='job.fabric' label='Fabric'/>
-                                                        </template>
-                                                    </TablerDropdown>
-                                                </template>
+                                <table class="table table-vcenter card-table">
+                                    <thead>
+                                        <tr>
+                                            <th>Name</th>
+                                            <th>Updated</th>
+                                            <th>Size</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr :key='job.id' v-for='job in jobs'>
+                                            <td @click='emitjob(job.job)'>
+                                                <span v-text='job.layer'/> - <span v-text='job.name'/>
+                                            </td>
+                                            <td v-text='fmt(job.updated)'></td>
+                                            <td class='d-flex'>
+                                                <span v-text='size(job.size)'/>
+                                                <div class='ms-auto btn-list'>
+                                                    <Download :auth='auth' :job='job' @login='$emit("login")' @perk='$emit("perk", $event)'/>
+                                                    <template v-if='auth && auth.access === "admin"'>
+                                                        <TablerDropdown>
+                                                            <slot>
+                                                                <SettingsIcon class='cursor-pointer'/>
+                                                            </slot>
+                                                            <template #dropdown>
+                                                                <TablerToggle @change='updateData(job)' v-model='job.fabric' label='Fabric'/>
+                                                            </template>
+                                                        </TablerDropdown>
+                                                    </template>
 
-                                                <HistoryIcon v-on:click.stop.prevent='emithistory(job.id)' class='cursor-pointer'/>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
+                                                    <HistoryIcon v-on:click.stop.prevent='emithistory(job.id)' class='cursor-pointer'/>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
 
-                            <div class='col-12 align-center'>
-                                OpenAddresses tracks free &amp; open data for <span v-text='location.name'/> including <span v-text='types.join(", ")'/>
-                            </div>
-                        </template>
+                                <div class='col-12 align-center'>
+                                    OpenAddresses tracks free &amp; open data for <span v-text='location.name'/> including <span v-text='types.join(", ")'/>
+                                </div>
+                            </template>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -78,6 +91,7 @@ import {
 import {
     TablerLoading,
     TablerDropdown,
+    TablerBreadCrumb,
     TablerToggle
 } from '@tak-ps/vue-tabler';
 import Download from './util/Download.vue';
@@ -153,6 +167,7 @@ export default {
         Coverage,
         TablerLoading,
         TablerDropdown,
+        TablerBreadCrumb,
         TablerToggle,
         HistoryIcon,
         SettingsIcon

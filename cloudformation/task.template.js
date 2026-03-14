3a71da6024656493ea5cf97a431a878ae1e0823c
Ian Dees <ian.dees@gmail.com>
2026-03-14 10:51:58 -0500

    Fix BatchLargeLaunchTemplate name collision (should be 'large' not 'mega')

--- cloudformation/task.template.js
+++ cloudformation/task.template.js
@@ -112,7 +112,7 @@ export default {
                         }
                     }]
                 },
-                LaunchTemplateName: 'mega'
+                LaunchTemplateName: 'large'
             }
         },
         BatchMegaLaunchTemplate: {

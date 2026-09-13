b6738e33b143154fb9420337d984d0a3ea93d5a3
ingalls <nick@ingalls.ca>
2026-09-13 13:50:35 -0600

    Implement unhead

--- api/web/src/pages/docs/main.js
+++ api/web/src/pages/docs/main.js
@@ -1,8 +1,10 @@
 import { createApp } from 'vue';
+import { createHead } from '@unhead/vue/client';
 
 import App from './App.vue';
 import std from '../../std.js';
 std();
 
 const app = createApp(App);
+app.use(createHead());
 app.mount('#app');
